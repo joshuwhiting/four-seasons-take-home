8bf4234f07e92b54b3beff400c05191cbd22041a
Joshua <joshaw807@gmail.com>
2026-09-03 12:50:49 -0400

    changed how the search works to be more robust to handle increase/decrease of hotels

--- pages/hotel_search_page.py
+++ pages/hotel_search_page.py
@@ -2,15 +2,11 @@ from pages.property_page import PropertyPage
 
 class HotelSearchPage(PropertyPage):
     URL = "https://www.fourseasons.com/find_a_hotel_or_resort/"
+    
+    def expand_region(self, region_name: str):
+        button = self.page.get_by_role("button", name=region_name)
+        if button.get_attribute("aria-expanded") == "false":
+            button.click()
 
-    def select_hotel(self, region_label, hotel_name):
-        region = self.page.get_by_role("region", name=region_label)
-
-        # if page is smaller, or mobile layout, the dropdown hides the hotels/resorts
-        if not region.is_visible():
-            self.page.get_by_role("button", name=region_label).click()
-
-        region.wait_for()
-        region.get_by_text(hotel_name, exact=True).click()
-            
-        
+    def select_property(self, property_link_name: str):
+        self.page.get_by_role("link", name=property_link_name).click()
--- tests/test_book_room.py
+++ tests/test_book_room.py
@@ -6,7 +6,8 @@ def test_can_search_and_select_hotel(page):
     search_page = HotelSearchPage(page)
     search_page.goto(HotelSearchPage.URL)
 
-    search_page.select_hotel(region_label="North America 55 properties", hotel_name="Los Cabos (Cabo Del Sol)")
+    search_page.expand_region("North America")
+    search_page.select_property("Los Cabos (Cabo Del Sol)")
 
     assert "cabo" in page.url.lower()
     page.get_by_role("navigation", name="Top Navigation").wait_for()
